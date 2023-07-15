a3fd5c81a0cbe8fd53e8395b3710f1f7bcd7ab32
Tabalamari <tabalamari@gmail.com>
2023-07-15 21:43:41 +0100

    Declaring a winner

--- src/App.js
+++ src/App.js
@@ -9,18 +9,28 @@ export default function Board() {
     const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
     function handleClick(i) {
-        if (squares[i]) {
+        const nextSquares = squares.slice();
+        calculateWinner(nextSquares)
+        // console.log(i)
+        // console.log(squares[i])
+        // console.log(Boolean(squares[i]))
+        // console.log(xIsNext)
+        if (
+            squares[i] || calculateWinner(nextSquares)
+        ) {
             return;
         }
-        const nextSquares = squares.slice();
+        
         if (xIsNext) {
             nextSquares[i] = "X";
         } else {
             nextSquares[i] = "O";
         }
-        // nextSquares[i] = "X";
         setSquares(nextSquares);
         setXIsNext(!xIsNext);
+        
+        // console.log(calculateWinner(nextSquares), "31 рядок")
+        // console.log((nextSquares), "squares")
 
     }
 
@@ -43,4 +53,31 @@ export default function Board() {
             </div>
         </>
     )
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if
+            (
+            (squares[a])
+            && 
+            (squares[a] === squares[b])
+            && 
+            (squares[a] === squares[c])
+        ) {
+            return squares[a];
+        }
+    }
+    return null;
 }
